e06d118d8cad9ecc20443f95229a165dbfd29d0a
Erin Moore <emoore@pillartechnology.com>
2018-11-14 13:43:19 -0500

    SMRT-703 autoformatting
    co-authored-by: Christopher J. McClellan <cmcclellan@pillartechnology.com>

--- lib/micro_service_watchinator/micro_service_watchinator.ex
+++ lib/micro_service_watchinator/micro_service_watchinator.ex
@@ -32,20 +32,23 @@ defmodule MicroServiceWatchinator.ConsumerWebsocketCheck do
 
   def do_check do
     Logger.info("Initiating web check against #{System.get_env("CONSUMER_URI")}")
+
     case WebSockex.start_link(System.get_env("CONSUMER_URI"), __MODULE__, %{}, async: false) do
       {:ok, data} ->
         Logger.info("Successfully Made Socket Connection")
 
         1
-        |> @metric_collector.count_metric("Opened", [{"ApplicationName", "Cota-Streaming-Consumer"}])
+        |> @metric_collector.count_metric("Opened", [
+          {"ApplicationName", "Cota-Streaming-Consumer"}
+        ])
         |> List.wrap()
         |> @metric_collector.record_metrics("Socket Connection")
 
         {:ok, data}
+
       {:error, err} ->
         Logger.warn("Failed to make a socket connection")
         {:ok, err}
     end
   end
-
 end
