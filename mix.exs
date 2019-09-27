647d38e4ac43e547bab983b58d20273d224ecea0
dunson062786@gmail.com <dunson062786@gmail.com>
2019-09-27 15:00:18 -0400

    Remove git submodule, and migrate chart
    smartcolumbusos/watchinator#14
    
    co-authored-by: Richard Jones <rajones@hntb.com>

--- mix.exs
+++ mix.exs
@@ -31,8 +31,8 @@ defmodule MicroServiceWatchinator.MixProject do
   defp deps do
     [
       {:websockex, "~> 0.4.0"},
-      {:streaming_metrics, path: "streaming_metrics"},
-      {:httpoison, "~> 0.11.1"},
+      {:streaming_metrics, "~> 2.2.0"},
+      {:httpoison, "~> 1.5.0"},
       {:cachex, "~> 3.0"},
       {:poison, "~> 4.0"},
       {:credo, "~> 0.10", only: [:dev, :test], runtime: false},
