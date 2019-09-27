ffe31df56d64e3313e3c3f3587bc5eb044b35e98
dunson062786@gmail.com <dunson062786@gmail.com>
2019-09-27 15:23:08 -0400

    remove main.tf and add travis.yml for open source ci
    smartcolumbusos/watchinator#14
    
    co-authored-by: Richard Jones <rajones@hntb.com>

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # MicroServiceWatchinator
 
 Watches micro-services running in the Smart Cities Data platform.
-
-Deploying to your favorite sandbox EKS can be done using terraform.
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MicroServiceWatchinator.MixProject do
   def project do
     [
       app: :micro_service_watchinator,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
